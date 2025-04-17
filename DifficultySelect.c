112b135a441ad88386870f3c2de5225e4987371c
MtMadness08 <soupeater56@gmail.com>
2025-04-17 18:30:02 -0400

    Push #1
    Built the selector code for difficulty. Going to work on the reading of the file next.

---
+++ DifficultySelect.c
@@ -0,0 +1,36 @@
+/*Select the difficulty*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+
+    //Select Difficulty
+    char buffer[20];
+    printf("Select Difficulty 1-3:\n1. Easy\n2. Normal\n3. Hard\n");
+    fgets(buffer,sizeof(buffer),stdin);
+    printf("\n");
+    
+    //Converts the buffer to an int to use the switch
+    int n = atoi(buffer);
+    // Checks for a valid input
+    if(n != 1 && n != 2 && n != 3){
+        printf("Invalid Input");
+        return 0;
+    }
+
+    switch(n){
+        case 1:
+            printf("Difficulty Selected: Easy\n");
+            break;
+        case 2:
+            printf("Difficulty Selected: Normal\n");
+            break;
+        case 3:
+            printf("Difficulty Selected: Hard\n");
+            break;
+    }
+
+}
---
+++ readEasyFile.c
@@ -0,0 +1,11 @@
+/*File to read the easy questions for the quiz*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main{
+    read
+
+}
