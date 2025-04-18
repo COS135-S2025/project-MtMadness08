b5afea37345db49244d35e8e2403a1ba23b7f25b
MtMadness08 <soupeater56@gmail.com>
2025-04-18 13:37:31 -0400

    update

--- readEasyFile.c
+++ readEasyFile.c
@@ -5,6 +5,27 @@
 #include <string.h>
 #include <ctype.h>
 
-#include readFiles.h
+int main(){
+    FILE* pokemon = fopen("EasyFile.txt","r+");
 
-Pokemon* question()
+    char buffer[1024];
+
+    while(fgets(buffer,sizeof(buffer),pokemon)!=NULL){
+        char *ptr = buffer + strlen(buffer) - 1;
+        //Trim Buffer
+
+        while(isspace(*ptr)) ptr--;
+
+        *(ptr+1)=0;
+        ptr = buffer;
+
+        while(isspace(*ptr)) ptr++;
+        
+
+        printf("%s\n", buffer);
+
+    }
+
+    fclose(pokemon);
+    printf("\n");
+}
