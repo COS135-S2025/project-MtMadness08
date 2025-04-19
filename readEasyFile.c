b34875aaae2ddb7fab72bb07bb9ef344d47d7e3a
MtMadness08 <soupeater56@gmail.com>
2025-04-19 14:55:30 -0400

    update

--- readEasyFile.c
+++ readEasyFile.c
@@ -7,24 +7,44 @@
 
 int main(){
     FILE* pokemon = fopen("EasyFile.txt","r+");
+    
+    int Guess = 0;
+    int Hint = 0;
 
     char buffer[1024];
+
+    while(Guess != 5){
+        char buffer[20];
+        printf("Type ""Hint"" for hint.\nType name of Pokemon to guess.\n");
+        fgets(buffer,sizeof(buffer),stdin);
+        printf("\n");
+        if(buffer == "Regice"){
+            Guess=Guess+1;
+            printf("Correct Answer\n");
+        }
+        
+
+    }
+
+    
+    /*if(fgets(buffer,sizeof(buffer),pokemon)!=NULL)
+        printf("%s\n", buffer);
 
     while(fgets(buffer,sizeof(buffer),pokemon)!=NULL){
         char *ptr = buffer + strlen(buffer) - 1;
         //Trim Buffer
 
-        while(isspace(*ptr)) ptr--;
+       while(isspace(*ptr)) ptr--;
 
-        *(ptr+1)=0;
-        ptr = buffer;
+       *(ptr+1)=0;
+       ptr = buffer;
 
-        while(isspace(*ptr)) ptr++;
+       while(isspace(*ptr)) ptr++;
         
 
-        printf("%s\n", buffer);
+       printf("%s\n", buffer);
 
-    }
+    }*/
 
     fclose(pokemon);
     printf("\n");
