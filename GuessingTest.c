2df28dc91423574030fef13b2c171b70231bd026
MtMadness08 <soupeater56@gmail.com>
2025-04-27 09:26:11 -0400

    working on taking in answers.

--- GuessingTest.c
+++ GuessingTest.c
@@ -3,5 +3,33 @@
 #include <string.h>
 #include <ctype.h>
 
-#include
+int main(){
 
+int i = 0;
+
+char arr[7][1024]={"1","2","3","4","5","6","Regice"};
+
+    char buffer[20];
+    printf("Print Pokemon Name\n");
+    fgets(buffer,sizeof(buffer),stdin);
+
+    while(i!=7){
+    if(strncmp(arr[6],buffer,6)==0){
+        printf("That was correct\n");
+        i++;
+    }
+    else{
+        printf("That was incorrect\n");
+        i++;
+    };}
+
+/*for(int i = 0; i<7; i++){
+    printf("%s",arr[i]);
+}*/
+
+}
+
+    /*char buffer[20];
+    printf("Select Difficulty 1-3:\n1. Easy\n2. Normal\n3. Hard\n");
+    fgets(buffer,sizeof(buffer),stdin);
+    printf("\n");*/
