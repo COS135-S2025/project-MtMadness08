3bdaa50b2d04fdad1ed73c592cfe9e99ed4719a8
MtMadness08 <soupeater56@gmail.com>
2025-04-29 19:51:49 -0400

    can make an array of items in a struct

--- writeTest.c
+++ writeTest.c
@@ -4,12 +4,33 @@
 #include <string.h>
 #include <ctype.h>
 
+typedef struct Text{
+    char string[20][20];
+}Text;
 int main(){
 
-    FILE* pokemon = fopen("test.txt","r+");
+    FILE* pokemon = fopen("test.txt","r");
+
+    Text first;
+
+    for(int j = 0; j<2;j++){
+        printf("Array #%d\n",j);
+    for (int i = 0; i<7; i++){
+        char buffer[1024];
+        if(fgets(buffer,sizeof(buffer),pokemon)!=NULL){
+            char *ptr = buffer + strlen(buffer) - 1;
+            //Trim Buffer
+            while(isspace(*ptr)) ptr--;
+            *(ptr+1)=0;
+            ptr = buffer;
+            while(isspace(*ptr)) ptr++;
+            }
+        strcpy(first.string[i],buffer);
+    }
     for(int i = 0; i<7; i++){
-    fprintf(pokemon,"\n");}
-    fclose(pokemon);
-
-
+        printf("%s\n",first.string[i]);
+    }
+    printf("\n");
+    
+}
 }
